ec44da2aa62240f969744bd11bb1036023469c30
wangdafang <wangyufang03@meituan.com>
2016-12-12 01:06:30 +0800

    all

--- find.sh
+++ find.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-#find <1G file and delete
-file=`find . -type f -size +100M -exec ls -l {} \;`  
-111111
-log=/var/log/
+#find <1M file and delete
+date=`date +%Y-%m-%d-%H:%M`
+log=`cd /var/log/`
+file=`find . -type f -size +1M -exec ls -l {} \;`  
+for size in $file ; do
+	echo "backup file and delete $size"
+	cp -a $size $date_$size.bak
+done
